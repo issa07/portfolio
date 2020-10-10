de3ac3427cf3f72b7d91eaae284c1306763e1850
Issa DEMIRCI <issa.demirci@epitech.eu>
2020-10-10 17:42:04 +0200

    portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,7 +5,7 @@ export const portfolioData = [
     languages: ['Html','Css'],
     languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: "Le site doit être visible depuis un ordinateur, une tablette ou un téléphone. Les images doivent toujours être affichées à 100%. C’est à dire que vous devez prévoir des images hautes et basses définitions. Le redimensionnement du navigateur (de manière à émuler un autre périphérique) doit par conséquent influer sur l’URL des images affichées.",
     picture: './media/maquette_responsive.png'
   },
   {
@@ -14,7 +14,7 @@ export const portfolioData = [
     languages: ['Html','Css'],
     languagesIcons: ['fab fa-html5','fab fa-css3-alt'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: "Une partie de ce projet est de reproduire une partie des fonctionalités présentes dans le framework Bootstrap. Vous devez reproduire toute celles décrites dans ce sujet. Elles ne doivent pas forcément avoir le même nom que dans Bootstrap, mais leur fonctionnement doit être similaire. Une fonctionnalité non finie ne sera pas comptée. Ne passez pas à une autre étape avant d’avoir fini toutes celles qui la précèdent, sinon elle ne comptera pas non plus.",
     picture: './media/framework_css.png'
   },
   {
@@ -23,7 +23,7 @@ export const portfolioData = [
     languages: ['Javascript', 'Css', 'Html'],
     languagesIcons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: "Vous allez devoir réaliser un jeu en utilisant la librairie jQuery reprenant les règles du célèbre jeu de Puissance 4.",
     picture: './media/puissance4.png'
   },
   {
@@ -32,7 +32,7 @@ export const portfolioData = [
     languages: ['Javascript','PHP','Css', 'Html'],
     languagesIcons: ['fab fa-html5', 'fab fa-css3-alt', 'fab fa-js','fab fa-php'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: "Le but du projet est de créer un site de rencontres à la manière d’un Meetic. Nous vous proposons de l’orienter vers la rencontre de personne dans un bahut qui partageraient des points communs par exemple. Il est obligatoire de programmer en PHP orienté objet. Il est fortement conseillé lors d’un envoi de données d’effectuer une requête ajax en JQuery afin de vérifier et envoyer les données, puis traiter la réponse. Il est obligatoire de gérer tous les cas d’erreurs possibles (injections SQL, doublons en DB, etc. . .).",
     picture: './media/my_meetic.png'
   },
   {
@@ -41,7 +41,7 @@ export const portfolioData = [
     languages: ['Javascript','Css', 'PHP', 'Html'],
     languagesIcons: ['fab fa-html5', 'fab fa-css3-alt', 'fab fa-js', 'fab fa-php', ],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: "À partir de la base de données MySQL fournie avec le sujet, vous allez devoir réaliser un site qui permettra d’interagir avec cette dernière. Ne passez pas trop de temps sur l’aspect visuel du site, le but du projet est orienté PHP et MySQL. Pensez bien à séparer le code permettant l’affichage et le code permettant la logique. Bien entendu, votre page devra être valide HTML5 et nous tiendrons compte de l’aspect général du site.",
     picture: './media/my_cinema.png'
   },
   {
