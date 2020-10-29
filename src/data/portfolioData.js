9b3f716f48068c339e250066754120b2699afcd3
Issa DEMIRCI <issa.demirci@epitech.eu>
2020-10-29 15:26:42 +0100

    portfolio2 ajouté

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -46,11 +46,11 @@ export const portfolioData = [
   },
   {
     id: 6,
-    name: 'test.io',
-    languages: ['javascript','react', 'css'],
+    name: 'Portfolio2',
+    languages: ['Javascript','React', 'Css'],
     languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: '/portfolio/media/project6.JPG'
+    source: 'https://github.com/issa07/portfolio2/tree/master',
+    info: "Il s'agit d'un deuxième site de portfolio que j'ai fait avec le Framework React en Material-Ui",
+    picture: '/portfolio/media/portfolio2.png'
   },
 ]
